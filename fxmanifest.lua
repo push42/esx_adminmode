be5feebd94630fec199c84230db803227e02075c
reverseHaze <96739561+reverseHaze@users.noreply.github.com>
2023-02-23 21:50:38 +0100

    1.0.0

---
+++ fxmanifest.lua
@@ -0,0 +1,35 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+description 'ESX Admin Mode'
+
+version '1.0'
+
+shared_script '@es_extended/imports.lua'
+
+client_scripts {
+	'client.lua',
+	'config.lua',
+}
+
+server_scripts {
+    "@mysql-async/lib/MySQL.lua",
+	'server.lua',
+	'config.lua',
+}
+
+files {
+    "html/index.html",
+    "html/script.js",
+    "html/style.css",
+    "html/jquery.datetimepicker.min.css",
+    "html/jquery.datetimepicker.full.min.js",
+    "html/date.format.js"
+}
+
+ui_page "html/index.html"
+
+dependencies {
+	'es_extended'
+}
